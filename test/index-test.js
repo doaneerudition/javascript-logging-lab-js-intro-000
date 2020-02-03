28aa8c70e0b4d3a694199816c3fbae6f77693b8e
Connor Doane <doaneerudition@gmail.com>
2020-02-03 17:50:36 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log("HALP!")', done => {
+  it('calls console.log("logger")', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
